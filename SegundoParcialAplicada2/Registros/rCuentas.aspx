01800121f00e389691aecca8186ab2ccf7a52efe
luis lorenzo <sixsixonelui@gmail.com>
2019-03-09 02:36:57 -0400

    cambio dal, cargando boostrap y demas , las master page y reg de cuentas.

---
+++ SegundoParcialAplicada2/Registros/rCuentas.aspx
@@ -0,0 +1,53 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="rCuentas.aspx.cs" Inherits="SegundoParcial.WebForm2" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <div class="container">
+        <h2 class="h2">Registro de Cuentas</h2>
+        <hr />
+        <form runat="server">
+
+            <div class="row">
+                <div class="form-group col-lg-3 col-sm-12">
+                    <asp:Label runat="server">Cuenta ID</asp:Label>
+                    <asp:TextBox runat="server" CssClass="form-control" ID="cuentaIdTextbox" TextMode="Number" Text="0" min="0"></asp:TextBox>
+                </div>
+                <div class="form-group col-lg-3 col-md-12">
+                    <br />
+                    <asp:Button ID="BuscarButton" runat="server" CssClass="btn btn-info col-lg-4 col-md-12" Text="Buscar" OnClick="BuscarButton_Click"/>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="form-group col-lg-4 col-sm-12">
+                    <asp:Label runat="server">Fecha</asp:Label>
+                    <asp:TextBox runat="server" CssClass="form-control" ID="fechaTextbox" TextMode="Date"></asp:TextBox>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="form-group col-lg-4 col-sm-12">
+                    <asp:Label runat="server">Nombre</asp:Label>
+                    <asp:TextBox runat="server" CssClass="form-control" ID="nombreTextbox" placeholder="Nombre"></asp:TextBox>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="form-group col-lg-4 col-sm-12">
+                    <asp:Label runat="server">Balance</asp:Label>
+                    <asp:TextBox runat="server" CssClass="form-control" ID="balanceTextbox" Text="0" min="0" ReadOnly="true"></asp:TextBox>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col-lg-5 col-sm-12">
+                    <asp:Button ID="NuevoButtton" runat="server" CssClass="btn btn-success col-lg-3 col-md-12" Text="Nuevo" OnClick="NuevoButton_Click"/>
+                    <asp:Button ID="GuardarButton" runat="server" CssClass="btn btn-warning col-lg-3 col-md-12" Text="Guardar" OnClick="GuardarButton_Click"/>
+                    <asp:Button ID="EliminarButton" runat="server" CssClass="btn btn-danger col-lg-3 col-md-12" Text="Eliminar" OnClick="EliminarButton_Click"/>
+                </div>
+            </div>
+
+        </form>
+    </div>
+</asp:Content>
+
---
+++ SegundoParcialAplicada2/Registros/rCuentas.aspx.cs
@@ -0,0 +1,144 @@
+﻿using BLL;
+using Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace SegundoParcialAplicada2.Registros
+{
+    public partial class rCuentas : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+        void Limpiar()
+        {
+            cuentaIdTextbox.Text = "0";
+            fechaTextbox.Text = string.Empty;
+            nombreTextbox.Text = string.Empty;
+            balanceTextbox.Text = "0";
+        }
+
+        private void LlenaCampos(CuentaBancaria cuenta)
+        {
+            cuentaIdTextbox.Text = cuenta.CuentaId.ToString();
+            fechaTextbox.Text = cuenta.Fecha.ToString("yyyy-MM-dd");
+            nombreTextbox.Text = cuenta.Nombre;
+            balanceTextbox.Text = cuenta.Balance.ToString();
+        }
+
+        private CuentaBancaria LlenaClase()
+        {
+            var cuenta = new CuentaBancaria();
+            cuenta.Fecha = Convert.ToDateTime(fechaTextbox.Text);
+            cuenta.Nombre = nombreTextbox.Text;
+            cuenta.Balance = 0;
+
+            return cuenta;
+        }
+
+        protected void NuevoButton_Click(object sender, EventArgs e)
+        {
+            Limpiar();
+        }
+
+        protected void GuardarButton_Click(object sender, EventArgs e)
+        {
+            int id = Convert.ToInt32(cuentaIdTextbox.Text);
+            if (!(String.IsNullOrEmpty(nombreTextbox.Text) || String.IsNullOrEmpty(fechaTextbox.Text)))
+            {
+                RepositorioBase<CuentaBancaria> repositorio = new RepositorioBase<CuentaBancaria>();
+                if (id == 0)
+                {
+                    repositorio.Guardar(LlenaClase());
+                    ScriptManager.RegisterStartupScript(Page, typeof(Page), "Popup", "alert('Guardado con Exito')", true);
+                }
+                else
+                {
+                    if (repositorio.Buscar(id) != null)
+                    {
+                        CuentaBancaria cuenta = repositorio.Buscar(int.Parse(cuentaIdTextbox.Text));
+
+                        cuenta.CuentaId = int.Parse(cuentaIdTextbox.Text);
+                        cuenta.Fecha = DateTime.Parse(fechaTextbox.Text);
+                        cuenta.Nombre = nombreTextbox.Text;
+                        cuenta.Balance = Decimal.Parse(balanceTextbox.Text);
+
+                        repositorio.Modificar(cuenta);
+                        ScriptManager.RegisterStartupScript(Page, typeof(Page), "Popup", "alert('Modificado con Exito')", true);
+                    }
+                    else
+                    {
+                        ScriptManager.RegisterStartupScript(Page, typeof(Page), "Popup", "alert('No existe una categoria con ese ID, no puede modificarse')", true);
+                    }
+                }
+            }
+            else if (id == 0)
+            {
+                ScriptManager.RegisterStartupScript(Page, typeof(Page), "Popup", "alert('Rellene todos los campos')", true);
+            }
+            Limpiar();
+        }
+
+        protected void EliminarButton_Click(object sender, EventArgs e)
+        {
+            int id = Convert.ToInt32(cuentaIdTextbox.Text);
+            if (id != 0)
+            {
+                RepositorioBase<CuentaBancaria> repositorio = new RepositorioBase<CuentaBancaria>();
+                if (repositorio.Buscar(id) != null)
+                {
+                    if (repositorio.Eliminar(id))
+                    {
+                        ScriptManager.RegisterStartupScript(Page, typeof(Page), "Popup", "alert('Eliminado con Exito')", true);
+                    }
+                    else
+                    {
+                        ScriptManager.RegisterStartupScript(Page, typeof(Page), "Popup", "alert('No se pudo eliminar')", true);
+                    }
+                    Limpiar();
+                }
+                else
+                {
+                    ScriptManager.RegisterStartupScript(Page, typeof(Page), "Popup", "alert('No existe')", true);
+                }
+            }
+            else
+            {
+                ScriptManager.RegisterStartupScript(Page, typeof(Page), "Popup", "alert('Seleccione un ID')", true);
+            }
+        }
+
+        protected void BuscarButton_Click(object sender, EventArgs e)
+        {
+            if (!String.IsNullOrEmpty(cuentaIdTextbox.Text))
+            {
+                int id = Convert.ToInt32(cuentaIdTextbox.Text);
+                if (id != 0)
+                {
+                    RepositorioBase<CuentaBancaria> repositorio = new RepositorioBase<CuentaBancaria>();
+                    CuentaBancaria CuentaBancaria = repositorio.Buscar(id);
+
+                    if (CuentaBancaria != null)
+                    {
+                        LlenaCampos(CuentaBancaria);
+                    }
+                    else
+                    {
+                        ScriptManager.RegisterStartupScript(Page, typeof(Page), "Popup", "alert('No existe')", true);
+                    }
+                }
+                else
+                {
+                    ScriptManager.RegisterStartupScript(Page, typeof(Page), "Popup", "alert('Seleccione un ID')", true);
+                }
+            }
+        }
+    }
+}
