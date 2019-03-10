d7dea06a9213199ab2f7c0a0397d3376bd597af8
luis lorenzo <sixsixonelui@gmail.com>
2019-03-10 13:13:37 -0400

    eliminando 2 reportes ya que los modifique y me sale error

--- SegundoParcialAplicada2/Reportes/ReportCuentaBancaria.aspx.designer.cs
+++ SegundoParcialAplicada2/Reportes/ReportCuentaBancaria.aspx.designer.cs
@@ -19,7 +19,16 @@ namespace SegundoParcialAplicada2.Reportes {
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.HtmlControls.HtmlForm form1;        
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+        
+        /// <summary>
+        /// ScriptManager1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.ScriptManager ScriptManager1;
         
         /// <summary>
         /// CuentaBancariaReportViewer control.
