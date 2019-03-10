9c75b3e921afa652aceba1af2cb309f7f34e96fb
luis lorenzo <sixsixonelui@gmail.com>
2019-03-10 13:44:38 -0400

    Arreglando reportes

---
+++ SegundoParcialAplicada2/Reportes/ReportDepositos.aspx
@@ -0,0 +1,25 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="ReportDepositos.aspx.cs" Inherits="SegundoParcialAplicada2.Reportes.ReportDepositos" %>
+
+<%@ Register assembly="Microsoft.ReportViewer.WebForms, Version=15.0.0.0, Culture=neutral, PublicKeyToken=89845dcd8080cc91" namespace="Microsoft.Reporting.WebForms" tagprefix="rsweb" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+             <%--ScriptManager--%>
+            <asp:ScriptManager ID="ScriptManager" runat="server"></asp:ScriptManager>
+
+
+           <%--Viewer--%>
+            <rsweb:ReportViewer ID="ElDepositosReportViewer" runat="server" ProcessingMode="Remote" Height="100%" Width="100%" >
+               
+            </rsweb:ReportViewer>
+        </div>
+    </form>
+</body>
+</html>
---
+++ SegundoParcialAplicada2/Reportes/ReportDepositos.aspx.cs
@@ -0,0 +1,30 @@
+﻿using ENTIDADES;
+using Microsoft.Reporting.WebForms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace SegundoParcialAplicada2.Reportes
+{
+    public partial class ReportDepositos : System.Web.UI.Page
+    {
+        Expression<Func<Deposito, bool>> filtro = p => true;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Page.IsPostBack)
+            {
+                ElDepositosReportViewer.ProcessingMode = ProcessingMode.Local;
+                ElDepositosReportViewer.Reset();
+                ElDepositosReportViewer.LocalReport.ReportPath = Server.MapPath(@"~\Reportes\ListDepositos.rdlc");
+                ElDepositosReportViewer.LocalReport.DataSources.Clear();
+                ElDepositosReportViewer.LocalReport.DataSources.Add(new ReportDataSource("CuentasDS", BLL.RepositorioDeposito.NDepositos(filtro)));
+                ElDepositosReportViewer.LocalReport.Refresh();
+            }
+        }
+    }
+}
